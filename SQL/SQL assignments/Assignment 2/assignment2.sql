8fd998cd62fd4700e07841ad6133a3b89ab23658
Muhammad Taha Malik <tahamalik567@gmail.com>
2024-03-18 00:51:22 +0500

    done sql assignment

--- SQL/SQL assignments/Assignment 2/assignment2.sql
+++ SQL/SQL assignments/Assignment 2/assignment2.sql
@@ -85,6 +85,63 @@ from pets
 group by kind
 having count(*)>5; 
 
+-- 12.Find the types of procedures that have an average cost greater than $50.
+select * from proceduresdetails;
+select avg(price) from proceduresdetails;
+select ProcedureType ,avg(price)
+from proceduresdetails
+group by ProcedureType
+having avg(price) > 50 ;
+
+-- 13.Classify pets as 'Young', 'Adult', or 'Senior' based on their age. Age less then 3 
+-- Young, Age between 3and 8 Adult, else Senior  
+select *,
+case
+	when Age <3 then 'Young'
+    when Age between 3 and 8 then 'Adult'
+    else 'Senior'
+end as age_group
+from pets;
+
+
+-- 14.Show the gender of pets with a custom label ('Boy' for male, 'Girl' for female). 
+select *,
+case
+	when Gender = 'male' then 'Boy'
+    when Gender = 'female' then 'Girl'
+end as boy_girl
+from pets;
+
+-- 15.For each pet, display the pet's name, the number of procedures they've had, and a 
+-- status label: 'Regular' for pets with 1 to 3 procedures, 'Frequent' for 4 to 7 
+-- procedures, and 'Super User' for more than 7 procedures. 
+select * from pets;
+select PetID, ProcedureType,count(ProcedureType) from procedureshistory
+group by PetID,ProcedureType;
+select pets.Name,pets.petID , count(procedureshistory.ProcedureType),
+case 
+	when count(procedureshistory.ProcedureType) between 1 and 3 then 'Regular'
+    when count(procedureshistory.ProcedureType) between 4 and 7 then 'Frequent'
+    else 'Super User'
+end as status_laybel
+from pets
+left join procedureshistory
+on pets.PetID = procedureshistory.PetID
+group by pets.Name,pets.PetID;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
