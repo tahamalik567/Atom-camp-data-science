52cf04ad6c0fb8f316e9a620fd65880e35057b90
Muhammad Taha Malik <tahamalik567@gmail.com>
2024-03-17 23:58:52 +0500

    updates

--- SQL/SQL assignments/Assignment 2/assignment2.sql
+++ SQL/SQL assignments/Assignment 2/assignment2.sql
@@ -47,7 +47,43 @@ on pets.PetID = procedureshistory.petID
 where procedureshistory.procedureType is null;
 
 -- 6. Find the name of the oldest pet
+ select max(Age) from pets;
+ select Name as oldest_pet
+ from pets 
+ where Age = (select max(Age) from pets);
  
+ -- 7. Find the details of procedures performed on 'Cuddles'.
+select Pets.PetID,Pets.Name, procedureshistory.procedureType
+from pets
+inner join procedureshistory
+on pets.PetID = procedureshistory.PetID
+where pets.Name = 'Cuddles';
+
+-- 8. List the pets who have undergone a procedure called 'VACCINATIONS'.
+select Pets.PetID,Pets.Name, procedureshistory.procedureType
+from pets
+inner join procedureshistory
+on pets.PetID = procedureshistory.PetID
+where procedureshistory.procedureType = 'VACCINATIONS';
+
+-- 9. Count the number of pets of each kind. 
+select * from pets;
+select distinct(kind) from pets;
+select  kind,count(*) as count_of_kind
+from pets
+group by kind;
+
+-- 10.Group pets by their kind and gender and count the number of pets in each group. 
+select  kind, gender,count(*) as count_of_pets
+from pets
+group by kind,gender;
+
+-- 11.Show the average age of pets for each kind, but only for kinds that have more than 5 
+-- pets.
+select kind,avg(age) 
+from pets 
+group by kind
+having count(*)>5; 
 
 
 
